b5252f7001cf8b007abda471bd8039610c5c8d58
BorissowT <tbs999borisov@gmail.com>
2020-11-18 19:13:23 +0300

    "initial"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        teamNameList: [
+          {"title":'House of Lannister – “Hear Me Roar"', "picture":'/logos/lannister_sigil_by_varvara64-d5ukyr3.png', 
+        "realTitle":"Lannister"},
+          {"title":'House Baratheon of Drangonstone – “Ours is the Fury”', "picture":'/logos/baratheon_of_dragonstone_sigil_by_varvara64-d5vl7k7.png', 
+          "realTitle":"Baratheon of Drangonstone"},
+          {"title":'House Targaryen – “Fire and Blood”', "picture":'/logos/targ.png', 
+          "realTitle":"Targaryen"},
+          {"title":'House of Stark – “Winter is Coming”', "picture":'/logos/stark.png', 
+          "realTitle":"Stark"},
+          {"title":'House Tyrell – “Growing Strong”', "picture":'/logos/tyrell.png', 
+          "realTitle":"Tyrell"},
+          {"title":'House of Arryn – “As High As Honor”', "picture":'/logos/arryn.png', 
+          "realTitle":"Arryn"},
+
+      ],
+        teamName: "",
+        teamPicture: "",
+        currentTeam: "",
+        text: ""
+      };
+
+      // This binding is necessary to make `this` work in the callback
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+      var teamName = event.target.value;
+      var teamPicture = "";
+      var text = "? Never heard of them?";
+      this.setState((state)=>{
+        if(event.target.value.length>3){
+          state.teamNameList.forEach(element => {
+            if(element.title.match(teamName)){
+              teamName = element.title;
+              teamPicture = element.picture;
+              text = "";
+            }
+        });
+      }
+        return {teamName: teamName, teamPicture: teamPicture, text: text}
+        });
+
+
+    }
+    render(){
+      return (
+        <div className="App">
+          <input 
+              type="text" 
+              placeholder="new flag" 
+              onChange={this.handleChange}
+            />
+          <h3>{this.state.teamName}{this.state.text}</h3>
+          <img src={this.state.teamPicture} />
+        </div>
+      );  
+      }
 }
+
 
 export default App;
